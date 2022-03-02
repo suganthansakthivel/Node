e3aa6dcd2a74a6dc7ff180f0676eb8fc9ec2d71e
Suganthan S <suganthans@SSPL.COM>
2022-03-02 11:08:45 +0530

    task

---
+++ orm/model.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+var sequelize=require('./connection');
+
+var author=sequelize.define('author',{
+    author_id:{
+      type: Sequelize.INTEGER,
+      primaryKey:true
+    },
+    name:{
+      type: Sequelize.TEXT,
+      allowNull:false
+    },
+    country:{
+      type: Sequelize.TEXT,
+      allowNull: true
+    }
+  });
+
+  var book=sequelize.define('book',{
+    book_id:{
+        type: Sequelize.INTEGER,
+        primaryKey:true
+    },
+    name:{
+        type: Sequelize.TEXT,
+        allowNull:false
+    },
+    category:{
+        type: Sequelize.TEXT,
+        allowNull:false
+    },
+    price:{
+        type: Sequelize.INTEGER,
+        allowNull:false
+    }
+})
+
+
+author.hasMany(book,{foreignKey: 'author_id'});
+book.belongsTo(author,{foreignKey: 'author_id'});
+
+
+
+  module.exports={book:book,author:author};
